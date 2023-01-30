0a2eaa4b24a35157df7d6a841f22d23fd3d55c23
clarice42 <claramaia820@gmail.com>
2023-01-29 23:14:26 -0300

    First commit

---
+++ app.ts
@@ -0,0 +1,17 @@
+const http = require("http");
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/books" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write("Hi there, there are no books available yet...");
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
+});
